53c982cb52fc8f675863af8fc792dc32460b5974
Matthias Arzt <arzt@mpi-cbg.de>
2018-06-04 14:25:32 +0200

    Add initial project files

---
+++ src/main/java/de/csbdresden/learnathon/command/HelloWorldCommand.java
@@ -0,0 +1,61 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+package de.csbdresden.learnathon.command;
+
+import net.imagej.Dataset;
+import net.imagej.ImageJ;
+import net.imagej.ops.OpService;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.RealType;
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.ui.UIService;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This example illustrates how to create an ImageJ {@link Command} plugin.
+ * <p>
+ * The code here is a simple Gaussian blur using ImageJ Ops.
+ * </p>
+ * <p>
+ * You should replace the parameter fields with your own inputs and outputs,
+ * and replace the {@link run} method implementation with your own logic.
+ * </p>
+ */
+@Plugin(type = Command.class, menuPath = "Plugins>Hello World")
+public class HelloWorldCommand<T extends RealType<T>> implements Command {
+
+	@Parameter
+	private UIService uiService;
+	
+    @Override
+    public void run() {
+    	uiService.show("Hello World");
+    }
+
+    /**
+     * This main function serves for development purposes.
+     * It allows you to run the plugin immediately out of
+     * your integrated development environment (IDE).
+     *
+     * @param args whatever, it's ignored
+     * @throws Exception
+     */
+    public static void main(final String... args) throws Exception {
+        // create the ImageJ application context with all available services
+        final ImageJ ij = new ImageJ();
+        ij.ui().showUI();
+    }
+
+}
